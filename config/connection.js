e4a4d6783faaac1eaff7e0955e70bea7d749e8c1
Yaroslav Volvach <60551329+YaroslavVolvach@users.noreply.github.com>
2023-02-28 18:48:16 -0500

    Update connection.js

--- config/connection.js
+++ config/connection.js
@@ -3,4 +3,4 @@ require('dotenv').config();
 
 const Sequelize = require('sequelize');
 
-module.exports =  new Sequelize('iblog_db', 'root', 'qwertyqwerty', {host: 'localhost', dialect: 'mysql'});
+module.exports =  new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {host: 'localhost', dialect: 'mysql'});
