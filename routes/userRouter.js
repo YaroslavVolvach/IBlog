f58181c21fe6116436be09c546548a0093675e9a
Yaroslav Volvach <60551329+YaroslavVolvach@users.noreply.github.com>
2023-02-28 22:08:55 -0500

    Update userRouter.js

--- routes/userRouter.js
+++ routes/userRouter.js
@@ -2,8 +2,6 @@ const router = require('express').Router()
 const {addUser, login, logout} = require('../controllers/userControllers')
 
 
-router.get('/', getUsers)
-router.get('/:id', getUser)
 router.post('/create', addUser)
 router.post('/login', login)
 router.post('/logout', logout)
