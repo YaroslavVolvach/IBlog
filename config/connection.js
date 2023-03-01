3929b480a18b54e7196808e756ca1a804a66972c
Yaroslav Volvach <60551329+YaroslavVolvach@users.noreply.github.com>
2023-02-28 22:43:34 -0500

    Update connection.js

--- config/connection.js
+++ config/connection.js
@@ -1,4 +1,4 @@
 const Sequelize = require('sequelize');
 require('dotenv').config();
 
-module.exports = new Sequelize(process.env.JAWSDB_URL, {dialect: 'mysql', url: process.env.DATABASE_URL})
+module.exports = new Sequelize(process.env.DATABASE_URL, {dialect: 'mysql'})
